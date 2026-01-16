b332976a41be3ff6817774a0c4fa787980039da2
dabananda <dabananda.dev@gmail.com>
2026-01-16 12:10:31 +0600

    feat: added employee entity

---
+++ src/main/java/com/dabananda/ems/entity/Employee.java
@@ -0,0 +1,29 @@
+package com.dabananda.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    
+    @Column(name = "password", nullable = false, unique = true)
+    private String email;
+}
